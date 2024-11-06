55237b5149825e9e3ff33b96a817c018a382b87c
Adabanya Christian <adabanyachristian21@gmail.com>
2024-11-07 00:16:37 +0100

    removed explicit recipeId

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,14 +2,14 @@ import Header from "../Components/Header";
 import Search from "../Components/Search";
 import RecipeList from "../Components/RecipeList";
 import { useState } from "react";
-export default function Homepage({ setRecipeId }) {
+export default function Homepage() {
   const [recipeData, setRecipeData] = useState([]);
 
   return (
     <div className="mx-auto">
       <Header />
       <Search setRecipeData={setRecipeData} />
-      <RecipeList recipeData={recipeData} setRecipeId={setRecipeId} />
+      <RecipeList recipeData={recipeData} />
     </div>
   );
 }
